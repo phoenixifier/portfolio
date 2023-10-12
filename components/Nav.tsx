3e35059bda4cdc6117a61e313071d4a8f56ceb2d
Rasul <imac@iMac-iMac.local>
2023-10-12 10:12:56 +0500

    feat:navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 // icons
 import {
@@ -28,7 +30,39 @@ export const navData = [
 ];
 
 const Nav: React.FC = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const path = router.pathname;
+  return (
+    <nav className="fixed bottom-0 z-50 mt-auto flex w-full flex-col items-center xl:right-[2%] xl:h-screen xl:w-16 xl:max-w-md xl:justify-center">
+      <div className="flex h-20 w-full items-center justify-between gap-y-10 bg-white/10 px-4 py-8 text-3xl backdrop-blur-sm md:px-40 xl:h-max xl:flex-col xl:justify-center xl:rounded-full xl:px-0 xl:text-xl">
+        {navData.map((link, index) => {
+          return (
+            <Link
+              className={`${link.path === path && 'text-accent'}
+              group relative flex items-center transition-all duration-300 hover:text-accent`}
+              key={index}
+              href={link.path}
+            >
+              {/* tooltip */}
+              <div className="absolute right-0 hidden pr-14 xl:group-hover:flex">
+                <div className="relative flex items-center rounded-[3px] bg-white p-1.5 text-primary">
+                  <div className="text-sm font-semibold capitalize leading-none">
+                    {link.name}
+                  </div>
+                  {/* triangle */}
+                  <div
+                    className="absolute -right-2
+                    border-y-[6px] border-l-8 border-solid border-y-transparent border-l-white"
+                  ></div>
+                </div>
+              </div>
+              {link.icon}
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
--- components/TopLeftImg.tsx
+++ components/TopLeftImg.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const TopLeftImg: React.FC = () => {
   return (
-    <div className="absolute left-0 top-0 z-10 w-[200px] mix-blend-color-dodge xl:w-[400px]">
+    <div className="absolute left-0 top-0 z-10 w-[200px] opacity-50 mix-blend-color-dodge xl:w-[400px]">
       <Image src="/top-left-img.png" width={400} height={400} alt="TopLeftImg" />
     </div>
   );
